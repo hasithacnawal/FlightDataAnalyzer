ee41403461814f8e09ebf459230951cd3354db10
Hasitha Nawalage <hnawalage@invorg.com>
2025-05-12 18:09:13 +0300

    Add project files.

---
+++ Controllers/FlightDataController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightDataAnalyzer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlightDataController : ControllerBase
+    {
+       
+
+        private readonly ILogger<FlightDataController> _logger;
+
+        
+    }
+}
---
+++ Models/FlightInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace FlightDataAnalyzer.Models
+{
+    public class FlightInfo
+    {
+        public int Id { get; set; }
+        public string AircraftRegistrationNumber { get; set; }
+        public string AircraftType { get; set; }
+        public string FlightNumber { get; set; }
+        public string DepartureAirport { get; set; }
+        public string DepartureDatetime { get; set; }
+        public string ArrivalAirport { get; set; }
+        public string ArrivalDatetime { get; set; }
+    }
+}
